b9dd69112875e7ac9f0b931c405581067dd2a782
mateogolf <rodriguezml11@mail.wlu.edu>
2017-08-13 15:42:42 -0400

    Examples of using templates and static

--- main/apps/blogs/views.py
+++ main/apps/blogs/views.py
@@ -4,16 +4,27 @@ from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect
 # the index function is called when root is visited
 def index(request):
-    response = "placeholder to later display all the list of blogs"
-    return HttpResponse(response)
+    context = {
+        "email": "blog@gmail.com",
+        "name": "mike"
+    }
+    return render(request, "blogs/index.html", context)
 
 def new(request):
     response = "placeholder to display a new form to create a new blog"
     return HttpResponse(response)
 
 def create(request):
-    response = "placeholder to display a new form to create a new blog"
-    return redirect('/blogs')
+    if request.method == "POST":
+        print "*" * 50
+        print request.POST
+        print request.POST['name']
+        print request.POST['desc']
+        request.session['name'] = "test"  # more on session below
+        print "*"*50
+        return redirect("/")
+    else:
+		return redirect("/")
 
 def show(request, number):
     response = "placeholder to display blog " + number
